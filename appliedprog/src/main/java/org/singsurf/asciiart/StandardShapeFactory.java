ccc521c3b7377c4910db6f2acb3fb7c222fe8503
ByteNadeem <nadeemzachav@hotmail.com>
2025-12-02 12:18:49 +0000

    Implement StandardShapeFactory for creating squares and containers

---
+++ appliedprog/src/main/java/org/singsurf/asciiart/StandardShapeFactory.java
@@ -0,0 +1,22 @@
+package org.singsurf.asciiart;
+
+/**
+ * A standard version of the shape factory creating normal Squares.
+ */
+public class StandardShapeFactory implements ShapeFactory {
+
+    @Override
+    public Square makeSquare(int size) {
+        return new Square(size);
+    }
+
+    @Override
+    public PositionedSquare makeSquare(int size,int x,int y) {
+        return new PositionedSquare(size,x,y);
+    }
+    @Override
+    public Container makeContainer() {
+        return new Container();
+    }
+
+}
